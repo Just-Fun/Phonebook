050e5bb30b1d684db6c363a51c8899ffe38fe4fe
Just-Fun <Just-Fun@users.noreply.github.com>
2016-10-24 21:29:53 +0300

    execute variables

--- src/main/java/ua/com/serzh/mysql_connection/ConnectionFactory.java
+++ src/main/java/ua/com/serzh/mysql_connection/ConnectionFactory.java
@@ -1,5 +1,6 @@
 package ua.com.serzh.mysql_connection;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -11,36 +12,45 @@ public class ConnectionFactory {
     private Properties properties;
     private static Connection connection;
 
+    String driver_class;
+    String db_url;
+    String user_name;
+    String user_password;
+
     // TODO look to Singleton, because this looks strange
     private static ConnectionFactory instance = new ConnectionFactory();
 
     private ConnectionFactory() {
         try {
             getProperties();
-            Class.forName(properties.getProperty("DRIVER_CLASS"));
+            Class.forName(driver_class);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     private void getProperties() {
-        try {
             properties = new Properties();
-            InputStream is = ConnectionFactory.class.getResourceAsStream("/db.properties");
+        try (InputStream is = ConnectionFactory.class.getResourceAsStream("/db.properties")) {
+//        try (FileInputStream is = new FileInputStream("/db.properties")) { // dont work...
             properties.load(is);
+
+            driver_class = properties.getProperty("DRIVER_CLASS");
+            db_url = properties.getProperty("DB_URL");
+            user_name = properties.getProperty("USER_NAME");
+            user_password = properties.getProperty("USER_PASSWORD");
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Some trouble with loading properties: " + e.getCause());
         }
     }
 
     private Connection createConnection() {
         try {
-            connection = DriverManager.getConnection(
-                    properties.getProperty("DB_URL"),
-                    properties.getProperty("USER_NAME"),
-                    properties.getProperty("USER_PASSWORD"));
+            connection = DriverManager.getConnection(db_url, user_name, user_password);
         } catch (SQLException e) {
-            e.printStackTrace();
+            connection = null;
+            throw new DatabaseManagerException(
+                    String.format("Cant get connection for db:%s user:%s", db_url, user_name), e);
         }
         return connection;
     }
