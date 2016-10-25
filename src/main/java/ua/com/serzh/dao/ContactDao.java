ba8ef817457270f937bf30173070159d90b9f05e
Just-Fun <Just-Fun@users.noreply.github.com>
2016-10-25 12:47:57 +0300

    add allUserContacts()

--- src/main/java/ua/com/serzh/dao/ContactDao.java
+++ src/main/java/ua/com/serzh/dao/ContactDao.java
@@ -10,6 +10,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import ua.com.serzh.entities.Contact;
+import ua.com.serzh.entities.ContactRowMapper;
 import ua.com.serzh.mysql_connection.ConnectionFactory;
 
 import java.sql.*;
@@ -17,10 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDao extends JdbcDaoSupport {
-    private static final String RETRIEVE_CONTACT_SQL = "SELECT contact_id, name, mobile_number, user_id FROM contacts WHERE user_id = ?";
-    private static final String SEARCH_BY_ID_SQL = "SELECT contact_id, name, mobile_number, user_id FROM contacts WHERE contact_id = ?";
-    private static final String UPDATE_SQL = "UPDATE contacts SET name = ?, mobile_number = ? WHERE contact_id = ?";
-    private static final String DELETE_SQL = "DELETE FROM contacts WHERE contact_id = ?";
 
     private Connection connection = null;
     private JdbcTemplate template;
@@ -36,65 +33,43 @@ public class ContactDao extends JdbcDaoSupport {
         return connection;
     }
 
-
     public ContactDao() {
     }
 
     public void insertContact(Contact contact) {
         getConnection2();
-
-        template.update("INSERT INTO contacts (name, mobile_number, user_id) VALUES (?, ?, ?)",
-                new Object[]{contact.getName(), contact.getMobileNumber(), contact.getUserId()});
+        String sql = "INSERT INTO contacts (name, mobile_number, user_id) VALUES (?, ?, ?)";
+        template.update(sql, new Object[]{contact.getName(), contact.getMobileNumber(), contact.getUserId()});
     }
 
     public List<Contact> searchContactByName(String name, int userId) {
         getConnection2();
-        String query = String.format("SELECT * FROM contacts WHERE user_id = %d AND name = %s", userId, name);
-
-        List<Contact> contacts = getJdbcTemplate().query(query, new BeanPropertyRowMapper(Contact.class));
-        return contacts;
+        String sql = String.format("SELECT * FROM contacts WHERE user_id = %d AND name = '%s'", userId, name);
+        return template.query(sql, new BeanPropertyRowMapper(Contact.class));
     }
 
     public Contact searchContactById(int contactId) {
-        Contact contact = new Contact();
-        try (PreparedStatement ps = getConnection2().prepareStatement(SEARCH_BY_ID_SQL)) {
-            ps.setInt(1, contactId);
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    contact.setContactId(rs.getInt("contact_id"));
-                    contact.setName(rs.getString("name"));
-                    contact.setMobileNumber(rs.getString("mobile_number"));
-                    contact.setUserId(rs.getInt("user_id"));
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return contact;
+        getConnection2();
+        String sql = "SELECT * FROM contacts WHERE contact_id = ?";
+        return template.queryForObject(sql, new Object[]{contactId}, new BeanPropertyRowMapper<>(Contact.class));
+    }
+
+    // just in case
+    public Contact searchContactById2(int contactId) {
+        getConnection2();
+        String sql = "SELECT * FROM contacts WHERE contact_id = ?";
+        return (Contact) template.queryForObject(sql, new Object[]{contactId}, new ContactRowMapper());
     }
 
     public List<Contact> allUserContacts(int userId) {
-        ArrayList<Contact> contacts = new ArrayList<>();
-        try (PreparedStatement ps = getConnection2().prepareStatement(RETRIEVE_CONTACT_SQL)) {
-            ps.setInt(1, userId);
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    Contact contact = new Contact();
-                    contact.setContactId(rs.getInt("contact_id"));
-                    contact.setName(rs.getString("name"));
-                    contact.setMobileNumber(rs.getString("mobile_number"));
-                    contact.setUserId(rs.getInt("user_id"));
-                    contacts.add(contact);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return contacts;
+        getConnection2();
+        String sql = String.format("SELECT * FROM contacts WHERE user_id = %d", userId);
+        return template.query(sql, new BeanPropertyRowMapper(Contact.class));
     }
 
     public void updateContact(Contact contact) {
-        try (PreparedStatement ps = getConnection2().prepareStatement(UPDATE_SQL)) {
+        String sql = "UPDATE contacts SET name = ?, mobile_number = ? WHERE contact_id = ?";
+        try (PreparedStatement ps = getConnection2().prepareStatement(sql)) {
             ps.setString(1, contact.getName());
             ps.setString(2, contact.getMobileNumber());
             ps.setInt(3, contact.getContactId());
@@ -105,8 +80,9 @@ public class ContactDao extends JdbcDaoSupport {
     }
 
     public void deleteContact(Contact contact) {
-        try (PreparedStatement ps = getConnection2().prepareStatement(DELETE_SQL)) {
-            ps.setInt(1, contact.getUserId()); // TODO check, hope its right
+        String sql = "DELETE FROM contacts WHERE contact_id = ?";
+        try (PreparedStatement ps = getConnection2().prepareStatement(sql)) {
+            ps.setInt(1, contact.getUserId()); // TODO check, hope its right, before was empty...
         } catch (SQLException e) {
             e.printStackTrace();
         }
