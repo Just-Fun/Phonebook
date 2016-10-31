8a591dae88477956a617f12e6db201e301744578
Just-Fun <Just-Fun@users.noreply.github.com>
2016-10-31 14:24:08 +0200

    create blanks for tests

---
+++ src/test/java/ua/com/serzh/dao/ContactDaoImplTest.java
@@ -0,0 +1,46 @@
+package ua.com.serzh.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Serzh on 10/31/16.
+ */
+public class ContactDaoImplTest {
+    @Test
+    public void insertContact() throws Exception {
+
+    }
+
+    @Test
+    public void searchContactByName() throws Exception {
+
+    }
+
+    @Test
+    public void searchContactById() throws Exception {
+
+    }
+
+    @Test
+    public void searchContactById2() throws Exception {
+
+    }
+
+    @Test
+    public void allUserContacts() throws Exception {
+
+    }
+
+    @Test
+    public void updateContact() throws Exception {
+
+    }
+
+    @Test
+    public void deleteContact() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/ua/com/serzh/service/ContactManagerTest.java
@@ -0,0 +1,26 @@
+package ua.com.serzh.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Serzh on 10/31/16.
+ */
+public class ContactManagerTest {
+    @Test
+    public void action() throws Exception {
+
+    }
+
+    @Test
+    public void pagination() throws Exception {
+
+    }
+
+    @Test
+    public void getAmountOfContacts() throws Exception {
+
+    }
+
+}
