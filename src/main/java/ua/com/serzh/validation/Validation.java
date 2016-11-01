5f4662d9d3d26cd54e3cb999974e61a92ca9a084
Just-Fun <Just-Fun@users.noreply.github.com>
2016-11-01 17:28:20 +0200

    add regex  fourLetters & fiveLetters

--- src/main/java/ua/com/serzh/validation/Validation.java
+++ src/main/java/ua/com/serzh/validation/Validation.java
@@ -5,16 +5,22 @@ public class Validation {
     private final static String phoneRegex = "^((\\+38)-?\\s?)(\\(?0\\d{2}\\)?)?-?\\s?\\d{3}-?\\s?\\d{2}-?\\s?\\d{2}$";
     private final static String fourLetters = "\\w{4,}";
 
+    private final static String fiveLetters = "\\w{5,}";
+
     public static boolean validate(String str, String regex) {
         boolean isCorrect = str.matches(regex);
         return isCorrect;
+    }
+
+    public static String getPhoneRegex() {
+        return phoneRegex;
     }
 
     public static String getFourLetters() {
         return fourLetters;
     }
 
-    public static String getPhoneRegex() {
-        return phoneRegex;
+    public static String getFiveLetters() {
+        return fiveLetters;
     }
 }
