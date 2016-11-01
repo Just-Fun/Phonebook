43c2025003bee25b7723d025293660ca066a9fab
Just-Fun <Just-Fun@users.noreply.github.com>
2016-11-01 23:22:26 +0200

    add 5 more column

--- src/main/java/ua/com/serzh/entities/Contact.java
+++ src/main/java/ua/com/serzh/entities/Contact.java
@@ -1,9 +1,19 @@
 package ua.com.serzh.entities;
 
+/**
+ * Created by Serzh on 10/25/16.
+ */
 public class Contact {
     private Integer contactId;
+    private String surname;
     private String name;
+    private String patronymic;
     private String mobileNumber;
+
+    private String homePhone;
+    private String address;
+    private String email;
+
     private Integer userId;
 
     public Contact() {
@@ -12,6 +22,26 @@ public class Contact {
     public Contact(String name, String mobileNumber, Integer userId) {
         this.name = name;
         this.mobileNumber = mobileNumber;
+        this.userId = userId;
+    }
+
+    public Contact(String name, String surname, String patronymic, String mobileNumber, Integer userId) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.mobileNumber = mobileNumber;
+        this.userId = userId;
+    }
+
+    public Contact(String surname, String name, String patronymic, String mobileNumber,
+                   String homePhone, String address, String email, Integer userId) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.mobileNumber = mobileNumber;
+        this.homePhone = homePhone;
+        this.address = address;
+        this.email = email;
         this.userId = userId;
     }
 
@@ -45,5 +75,45 @@ public class Contact {
 
     public void setUserId(Integer userId) {
         this.userId = userId;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- src/main/java/ua/com/serzh/entities/ContactRowMapper.java
+++ src/main/java/ua/com/serzh/entities/ContactRowMapper.java
@@ -4,17 +4,58 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+/**
+ * Created by Serzh on 10/27/16.
+ */
 public class ContactRowMapper implements RowMapper {
     @Override
     public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
         Contact contact = new Contact();
 
         contact.setContactId(rs.getInt("contact_id"));
-        contact.setMobileNumber(rs.getString("mobile_number"));
+        contact.setSurname(rs.getString("surname"));
         contact.setName(rs.getString("name"));
+        contact.setPatronymic(rs.getString("patronymic"));
+        contact.setMobileNumber(rs.getString("mobile_number"));
+
+        contact.setHomePhone(rs.getString("home_phone"));
+        contact.setAddress(rs.getString("address"));
+        contact.setEmail(rs.getString("email"));
+
         contact.setUserId(rs.getInt("user_id"));
+
 
         return contact;
     }
+
+   /* public void insertNamedParameter(Contact contact){
+
+        String sql = "INSERT INTO CUSTOMER " +
+                "(CUST_ID, NAME, AGE) VALUES (:custId, :name, :age)";
+
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("custId", customer.getCustId());
+        parameters.put("name", customer.getName());
+        parameters.put("age", customer.getAge());
+
+        getSimpleJdbcTemplate().update(sql, parameters);
+
+    }*/
+
+    /*public static final RowUnmapper<Contact> ROW_UNMAPPER = new RowUnmapper<Comment>() {
+        @Override
+        public Map<String, Object> mapColumns(Comment comment) {
+            Map<String, Object> mapping = new LinkedHashMap<String, Object>();
+            mapping.put("id", comment.getId());
+            mapping.put("user_name", comment.getUserName());
+            mapping.put("contents", comment.getContents());
+            mapping.put("created_time", new java.sql.Timestamp(comment.getCreatedTime().getTime()));
+            mapping.put("favourite_count", comment.getFavouriteCount());
+            return mapping;
+        }
+    };*/
 }
