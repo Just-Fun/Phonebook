29d9b246e667da13dae0a6ec3b17be746c13f174
Just-Fun <Just-Fun@users.noreply.github.com>
2016-11-04 03:38:54 +0200

    clean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,18 +10,6 @@ import ua.com.serzh.entities.User;
  */
 public class App {
     public static void main(String[] args) {
-       /* ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("WEB-INF/application-context.xml");
-//        ApplicationContext context = new ClassPathXmlApplicationContext("webapp/WEB-INF/application-context.xml");
 
-        UserDaoImpl cust = (UserDaoImpl) context.getBean("userDao");
-        System.out.println(cust.getJdbcTemplate());
-        System.out.println(cust);
-
-        UserDao userDao = (UserDao) context.getBean("userDao");
-        User user = new User("VaniaTest8", "pass4");
-        userDao.insertUser(user);
-
-        User searchByNameAndPassword = userDao.searchByNameAndPassword("VaniaTest6", "pass4");
-        System.out.println(searchByNameAndPassword.getName());*/
     }
 }
--- src/main/java/ua/com/serzh/entities/ContactRowMapper.java
+++ src/main/java/ua/com/serzh/entities/ContactRowMapper.java
@@ -28,34 +28,6 @@ public class ContactRowMapper implements RowMapper {
 
         contact.setUserId(rs.getInt("user_id"));
 
-
         return contact;
     }
-
-   /* public void insertNamedParameter(Contact contact){
-
-        String sql = "INSERT INTO CUSTOMER " +
-                "(CUST_ID, NAME, AGE) VALUES (:custId, :name, :age)";
-
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("custId", customer.getCustId());
-        parameters.put("name", customer.getName());
-        parameters.put("age", customer.getAge());
-
-        getSimpleJdbcTemplate().update(sql, parameters);
-
-    }*/
-
-    /*public static final RowUnmapper<Contact> ROW_UNMAPPER = new RowUnmapper<Comment>() {
-        @Override
-        public Map<String, Object> mapColumns(Comment comment) {
-            Map<String, Object> mapping = new LinkedHashMap<String, Object>();
-            mapping.put("id", comment.getId());
-            mapping.put("user_name", comment.getUserName());
-            mapping.put("contents", comment.getContents());
-            mapping.put("created_time", new java.sql.Timestamp(comment.getCreatedTime().getTime()));
-            mapping.put("favourite_count", comment.getFavouriteCount());
-            return mapping;
-        }
-    };*/
 }
