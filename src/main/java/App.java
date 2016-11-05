d7307fa47062647ee4def583c8a34fc700cf345f
Just-Fun <Just-Fun@users.noreply.github.com>
2016-11-05 22:32:41 +0200

    split on different folders

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,3 @@
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ua.com.serzh.dao.UserDao;
-import ua.com.serzh.dao.UserDaoImpl;
-import ua.com.serzh.entities.User;
-
 /**
  * Created by Serzh on 10/25/16.
  */
