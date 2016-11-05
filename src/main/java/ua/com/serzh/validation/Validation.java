5f387cd15634f179344b00dc0d36991aac94cb83
Just-Fun <Just-Fun@users.noreply.github.com>
2016-11-05 15:13:00 +0200

    add few regex

--- src/main/java/ua/com/serzh/validation/Validation.java
+++ src/main/java/ua/com/serzh/validation/Validation.java
@@ -6,8 +6,12 @@ package ua.com.serzh.validation;
 public class Validation {
 
     private final static String PHONE_REGEX = "^((\\+38)-?\\s?)(\\(?0\\d{2}\\)?)?-?\\s?\\d{3}-?\\s?\\d{2}-?\\s?\\d{2}$";
+
+    private final static String THREE_LETTERS = "[A-z]{3,}";
     private final static String FOUR_LETTERS = "[A-z]{4,}";
     private final static String FIVE_LETTERS = "[A-z]{5,}";
+
+    private final static String FIVE_LETTERS_OR_DIGITS = "\\w{5,}";
 
     private static final String EMAIL_PATTERN =
             "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
@@ -25,11 +29,19 @@ public class Validation {
         return EMAIL_PATTERN;
     }
 
+    public static String getThreeLetters() {
+        return THREE_LETTERS;
+    }
+
     public static String getFourLettersPattern() {
         return FOUR_LETTERS;
     }
 
     public static String getFiveLettersPattern() {
         return FIVE_LETTERS;
+    }
+
+    public static String getFiveLettersOrDigits() {
+        return FIVE_LETTERS_OR_DIGITS;
     }
 }
